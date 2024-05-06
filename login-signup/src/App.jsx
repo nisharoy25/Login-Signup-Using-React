6c31a14d2b19fa3e7f07b887b7892e5f0315557c
Nisharoy25 <roynisha1929@gmail.com>
2024-05-06 23:34:53 +0530

    initial commit

---
+++ login-signup/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import RegisterLogin from './Components/RegisterLogin';
+
+
+function App() {
+
+  return (
+  <div>
+  <RegisterLogin></RegisterLogin>
+  </div>
+       
+ );
+}
+
+export default App
